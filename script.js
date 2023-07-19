34170ba74c7885f490cf9bbd791d740e3b71143c
Jari <zalmanhajari@gmail.com>
2023-07-19 23:02:49 +0800

    can display now

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const gameContainer = document.querySelector(".game-container");
+const cellTemplate = gameContainer.querySelector(".cell");
+
 const gameBoard = (() => {
+  console.log("wow");
   const board = [];
   for (let i = 0; i < 9; i++) {
-    board.push("");
+    if (Math.round(Math.random()) === 0) board.push("X");
+    else board.push("O");
   }
 
   return { board };
+})();
+
+const displayController = (() => {
+  const board = gameBoard.board;
+  const display = () => {
+    // clears board
+    gameContainer.innerHTML = "";
+    board.forEach((element) => {
+      const cell = cellTemplate.cloneNode(true);
+      console.log(element);
+      const symbol = (cell.querySelector(".symbol").innerText = element);
+      gameContainer.appendChild(cell);
+    });
+  };
+
+  return { display };
 })();
 
 const Player = (sign) => {
@@ -13,4 +34,7 @@ const Player = (sign) => {
 
 playerX = Player("X");
 console.log(playerX.sign);
-console.log(gameBoard.board);
+displayController.display();
+//console.log(gameBoard.board);
+
+cellTemplate.remove();
