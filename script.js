c7cfc95c896cc60a18dca82c49968e10cf278dc8
Jari <zalmanhajari@gmail.com>
2023-08-05 01:38:17 +0800

    switched to 2d array and added the gamemanager

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const gameContainer = document.querySelector(".game-container");
 const cellTemplate = gameContainer.querySelector(".cell");
 
+const Cell = () => {
+  let value = "";
+  const addToken = (player) => {
+    value = player;
+  };
+  const getValue = () => {
+    return value;
+  };
+  return { addToken, getValue };
+};
+
 const gameBoard = (() => {
   console.log("wow");
-  const board = [];
-  for (let i = 0; i < 9; i++) {
-    if (Math.round(Math.random()) === 0) board.push("X");
-    else board.push("O");
+  let board = [];
+  for (let i = 0; i < 3; i++) {
+    board[i] = [];
+    for (let j = 0; j < 3; j++) {
+      board[i][j] = Cell();
+    }
   }
 
   return { board };
@@ -17,12 +30,17 @@ const displayController = (() => {
   const display = () => {
     // clears board
     gameContainer.innerHTML = "";
-    board.forEach((element) => {
-      const cell = cellTemplate.cloneNode(true);
-      console.log(element);
-      const symbol = (cell.querySelector(".symbol").innerText = element);
-      gameContainer.appendChild(cell);
-    });
+    console.log(board);
+    //for ever row
+    for (let i = 0; i < board.length; i++) {
+      //Get the cell of each row
+      board[i].forEach((element) => {
+        const cell = cellTemplate.cloneNode(true);
+        const symbol = (cell.querySelector(".symbol").innerText =
+          element.getValue());
+        gameContainer.appendChild(cell);
+      });
+    }
   };
 
   return { display };
@@ -32,9 +50,60 @@ const Player = (sign) => {
   return { sign };
 };
 
+const gameManager = (() => {
+  playerX = Player("X");
+  playerO = Player("O");
+  const board = gameBoard.board;
+
+  let gameOver = false;
+
+  const players = [playerX, playerO];
+
+  let activePlayer = players[0];
+
+  // this will change the player by +1 to the index
+  // if the +1 overflows the array we loop back
+  // I did this so if we ever decided to add a third player
+  // we can keep using this code.
+  const switchPlayer = () => {
+    if (players.length - 1 < players.indexOf(activePlayer) + 1) {
+      console.log("true");
+      activePlayer = players[0];
+    } else {
+      console.log("normal transfer");
+      activePlayer = players[players.indexOf(activePlayer) + 1];
+    }
+  };
+
+  const playGame = () => {
+    while (!gameOver) {
+      //get input
+      playRound(activePlayer);
+      gameOver = true;
+    }
+  };
+
+  //---------------- FINISH THISS ------------------------
+  const playRound = (player) => {
+    let row = prompt("row");
+    console.log("row");
+    console.log(row);
+
+    let column = prompt("column");
+    console.log("column");
+    console.log(column);
+  };
+
+  return { playGame };
+})();
+
+//console.log(gameBoard.board);
+
 playerX = Player("X");
+playerO = Player("O");
 console.log(playerX.sign);
 displayController.display();
-//console.log(gameBoard.board);
+
+gameManager.playGame();
 
 cellTemplate.remove();
